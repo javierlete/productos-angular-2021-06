123f002d22a6ea1308311a3c7fa68bc06379c278
Javier <jglete@eskura21.com>
2021-06-15 10:17:06 +0200

    Mensajes de error y de éxito para productos

--- src/app/producto.service.ts
+++ src/app/producto.service.ts
@@ -3,36 +3,64 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { MensajeService } from './mensaje.service';
 import { Producto } from './producto';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductoService {
-  
+
   private url = 'http://localhost:3000/productos/';
 
-  constructor(private http: HttpClient, private mensajeService: MensajeService) {}
+  constructor(private http: HttpClient, private mensajeService: MensajeService) { }
 
   obtenerProductos(): Observable<Producto[]> {
     return this.http.get<Producto[]>(this.url).pipe(
-      tap(_ => this.mensajeService.nuevo({ texto: 'Se han obtenido los productos', nivel: 'success' }))
+      tap(_ => this.mensajeService.nuevo({ texto: 'Se han obtenido los productos', nivel: 'success' })),
+      catchError(() => {
+        this.mensajeService.nuevo({ texto: 'Error al obtener los productos', nivel: 'danger' })
+        return [];
+      })
     );
   }
 
   obtenerProductoPorId(id: number): Observable<Producto> {
-    return this.http.get<Producto>(this.url + id);
+    return this.http.get<Producto>(this.url + id).pipe(
+      tap(_ => this.mensajeService.nuevo({ texto: 'Se ha obtenido el producto ' + id, nivel: 'success' })),
+      catchError(() => {
+        this.mensajeService.nuevo({ texto: 'Error al obtener el producto ' + id, nivel: 'danger' })
+        return [];
+      })
+    );
   }
 
   insertar(producto: Producto): Observable<Producto> {
-    return this.http.post<Producto>(this.url, producto);
+    return this.http.post<Producto>(this.url, producto).pipe(
+      tap(_ => this.mensajeService.nuevo({ texto: 'Se ha insertado el producto ' + producto.nombre, nivel: 'success' })),
+      catchError(() => {
+        this.mensajeService.nuevo({ texto: 'Error al insertar el producto ' + producto.nombre, nivel: 'danger' })
+        return [];
+      })
+    );
   }
-  
+
   modificar(producto: Producto): Observable<Producto> {
-    return this.http.put<Producto>(this.url + producto.id, producto);
+    return this.http.put<Producto>(this.url + producto.id, producto).pipe(
+      tap(_ => this.mensajeService.nuevo({ texto: 'Se ha modificado el producto ' + producto.id, nivel: 'success' })),
+      catchError(() => {
+        this.mensajeService.nuevo({ texto: 'Error al modificar el producto ' + producto.id, nivel: 'danger' })
+        return [];
+      })
+    );
   }
 
   borrarProducto(id: number): Observable<any> {
-    return this.http.delete(this.url + id);
+    return this.http.delete(this.url + id).pipe(
+      tap(_ => this.mensajeService.nuevo({ texto: 'Se ha borrado el producto ' + id, nivel: 'success' })),
+      catchError(() => {
+        this.mensajeService.nuevo({ texto: 'Error al borrar el producto ' + id, nivel: 'danger' })
+        return [];
+      })
+    );
   }
 }
